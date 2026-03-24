f5d609177d4e3e10dbf824bcb2af5833701140ac
3clip5e <sblackeclipse@gmail.com>
2026-03-24 21:36:33 +0100

    Fix: Simplifier la configuration Vite pour corriger l'erreur de build

--- vite.config.js
+++ vite.config.js
@@ -5,24 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.')
-          const ext = info[info.length - 1]
-          if (/\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/i.test(assetInfo.name)) {
-            return `assets/[name]-[hash][extname]`
-          }
-          if (/\.(png|jpe?g|gif|svg|webp|avif)(\?.*)?$/i.test(assetInfo.name)) {
-            return `assets/[name]-[hash][extname]`
-          }
-          if (/\.(woff2?|eot|ttf|otf)(\?.*)?$/i.test(assetInfo.name)) {
-            return `assets/[name]-[hash][extname]`
-          }
-          return `assets/[name]-[hash][extname]`
-        }
-      }
-    }
+    assetsDir: 'assets'
   }
 })
